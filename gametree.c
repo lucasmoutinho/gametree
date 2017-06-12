bcc1f0d508e7ef8b91b7c77ca6999b7bba07671c
Lucas Moutinho <lucasmoutinho>
2017-06-12 20:40:37 -0300

    Add struct no

--- gametree.c
+++ gametree.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct no{
+	int tabuleiro[2][7];
+	struct no* esq;
+	struct no* dir;
+} t_no;
+
+t_no* criaNo(){
+	t_no* no = (t_no*)malloc(sizeof(t_no));
+	no->esq = NULL;
+	no->dir = NULL;
+	return no;
+}
 
 int main(){
 
-	
+
 	return 0;
 }
